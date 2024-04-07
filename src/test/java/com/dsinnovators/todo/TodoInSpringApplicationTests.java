f46e76b2136dc2a5ff5441f138b5407bb8fb64dc
Md. Abdus Salam <salaam.mbstu@gmail.com>
2024-04-07 23:51:55 +0600

    initial project

---
+++ src/test/java/com/dsinnovators/todo/TodoInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsinnovators.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
