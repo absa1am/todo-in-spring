244e99686a6aba7b61440886860d240276c2cad9
Md. Abdus Salam <salaam.mbstu@gmail.com>
2024-04-09 02:58:02 +0600

    feat: add task controller and page

---
+++ src/main/java/com/dsinnovators/todo/controllers/TaskController.java
@@ -0,0 +1,14 @@
+package com.dsinnovators.todo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+
+    @GetMapping("/")
+    public String index() {
+        return "pages/index";
+    }
+
+}
